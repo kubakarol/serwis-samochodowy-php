b4fd2a3d4c01933d1a36e31da417151d64ea3353
igorStudia <igorpuscikowskibiznes@gmail.com>
2024-01-08 20:50:58 +0100

    wrzucenie projektu na github

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+//    zwracanie widoku w zaleznosci od rodzaju konta 0 =user, 1=admin
+        public function index()
+    {
+        if(Auth::id()){
+            $konto = Auth()->user()->konto;
+            if($konto==0){
+            return view('user');
+            }
+            else if($konto==1){
+            return view('admin');
+            }
+            else{
+                return view('strona');
+            }
+            }
+
+    }
+    
+}
